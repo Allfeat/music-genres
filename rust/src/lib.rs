2f1546101b15f6e95c45ee382337b7601bce287c
SailorSnoW <sailorsnow@pm.me>
2025-07-30 03:01:33 +0200

    fix no_std usage

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,8 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![cfg_attr(not(feature = "std"), no_std)]
-
 mod generated;
 
 #[cfg(feature = "js")]
